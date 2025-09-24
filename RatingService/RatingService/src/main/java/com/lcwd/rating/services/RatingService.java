753ae98f1d3955a0f9441d4deb7a23e33a27f50b
ANUJ <ak5338837@gmail.com>
2025-09-25 03:00:40 +0530

    Initial commit: Added all microservices

---
+++ RatingService/RatingService/src/main/java/com/lcwd/rating/services/RatingService.java
@@ -0,0 +1,26 @@
+package com.lcwd.rating.services;
+
+import com.lcwd.rating.entities.Rating;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface RatingService {
+
+
+    //create
+    Rating create(Rating rating);
+
+    //get all ratings
+    List<Rating> getRatings();
+
+
+
+    //get all by userid
+    List<Rating> getRatingByUserId(String userId);
+
+
+    //get by hotelId
+    List<Rating> getRatingByHotelId(String hotelId);
+
+}
